4a6d9f5c6e412127f246dce6d9f15a91812691d9
EGPAerospace <egp.aerospace@gmail.com>
2024-01-21 19:23:41 +0100

    Build Folder in gitignore

---
+++ app/source_parser.lua
@@ -0,0 +1,36 @@
+-- Class for extracting import function calls from source files
+return function(filename)
+    -- Ensuring that there is an empty blank line at the end of the source code.
+    -- If not, the bundled file will have endend and it will fail to run.
+    local file_append = io.open(filename, "a")
+    file_append:write("\n")
+    file_append:close()
+
+    local file = io.open(filename, "r")
+    if file == nil then
+        error("File not found: " .. filename)
+    end
+    local file_content = file:read("*a")
+    file:close()
+    local included_files = {}
+    
+    -- Search for import() calls with dobule quotes (!)
+    for f in string.gmatch(file_content, 'import%(["\']([^\'"]-)["\']%)') do
+        table.insert(included_files, f)
+    end
+    
+    self = {}
+    self.filename = filename
+    self.content = file_content
+    self.includes = included_files
+    return self
+end
+
+
+
+
+
+
+
+
+
